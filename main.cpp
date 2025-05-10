69af7b402844709851511328a21e68fd3290f8bb
Halabala7 <117306958+Halabala7@users.noreply.github.com>
2025-05-10 02:05:22 +0200

    Update main.cpp
    Przebudowa struktury programu, w celu usprawnienia elastyczności działania klas

--- main.cpp
+++ main.cpp
@@ -6,12 +6,62 @@
 #include <limits>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
-
+class Game;
+class Ship;
 class Board;
+class Player;
+class Human;
+class Si;
+
+class Game
+{
+    private:
+        void startGameVsSi();
+        void startGameVsHuman();
+        bool chooseShipPlacement();
+
+
+    public:
+        Player* player1;
+        Player* player2;
+
+        Game(int gamemode);
+        ~Game();
+
+
+
+};
+class Board
+{
+    private:
+        char map[10][10];
+        vector<Ship> ships;
+    public:
+        Board();
+        void printMap();
+        bool autoPlaceShip(int size);
+        bool checkShipPlace(int randomX, int randomY, int isVertical, int size);
+        void addShipToBoard(const Ship& ship);
+        void printShips();
+};
 
 class Player
 {
+    public:
+        string name;
+        bool manualShipPlacement(Board& board);
+        bool autoShipPlacement(Board& board);
+        virtual bool makeMove() {return false;};
 
+};
+class Human : public Player
+{
+    bool makeMove() override;
+
+};
+class Si : public Player
+{
+    bool makeMove() override;
 
 };
 class Ship
@@ -34,20 +84,106 @@ class Ship
         bool isSunk();
 
 };
-
-class Board
+Game::Game(int gamemode)
 {
-    private:
-        char map[10][10];
-        vector<Ship> ships;
-    public:
-        Board();
-        void printMap();
-        bool autoPlaceShip(int size);
-        bool checkShipPlace(int randomX, int randomY, int isVertical, int size);
-        void addShipToBoard(const Ship& ship);
-        void printShips();
-};
+    // gra przeciwko SI
+    if(gamemode == 1)
+    {
+        player1 = new Human();
+        player2 = new Si();
+        startGameVsSi();
+
+    }
+    // gra na dwoch graczy
+    else if(gamemode == 2)
+    {
+        player1 = new Human();
+        player2 = new Human();
+    }
+}
+Game::~Game()
+{
+    delete player1;
+    delete player2;
+}
+void Game::startGameVsSi()
+{
+    cout<<"wybrales gre z SI\n";
+    cout<<"Czy chcesz ustawic samodzielnie statki? t/n"<<endl;
+
+    //tworzenie mapy czlowieka
+    Board humanBoard;
+
+    //manualne wstawianie statkow
+    if(chooseShipPlacement())
+    {
+        cout<<"w trakcie tworzenia"<<endl;
+        ExitProcess(0);
+    }
+    //automatyczne wstawianie statkow
+    else if(!chooseShipPlacement())
+    {
+        Board humanBoard;
+
+        player1->autoShipPlacement(humanBoard);
+        cout<<"plansza gracza: \n";
+        humanBoard.printMap();
+
+        cout<<endl;
+
+    }
+    //tworzenie mapy Si
+    Board siBoard;
+    player2->autoShipPlacement(siBoard);
+    cout<<"statki si postawione pomyslnie!\n";
+
+    cout<<"plansza si: \n";
+    siBoard.printMap();
+}
+void Game::startGameVsHuman()
+{
+    cout<<"Opcja w trakcie tworzenia"<<endl;
+    system("cls");
+}
+bool Game::chooseShipPlacement()
+{
+    char placeChoice;
+    while (true)
+    {
+
+
+         if (cin>>placeChoice)
+         {
+            if (placeChoice == 't' || placeChoice == 'n')
+            {
+                break;
+            }
+            cout << "musisz podac t lub n!\n";
+         }
+         else
+         {
+            cout << "To nie jest litera!\n";
+            cin.clear(); // Wyczysc flage bledu
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Zignoruj reszte danych
+         }
+    }
+    if(placeChoice == 't')
+    {
+        return true;
+    }
+    else if(placeChoice == 'n')
+    {
+        return false;
+    }
+}
+bool Human::makeMove()
+{
+    return false;
+}
+bool Si::makeMove()
+{
+    return false;
+}
 Ship::Ship(Board& board, int size)
 {
 
@@ -67,13 +203,13 @@ Ship::Ship(Board& board, int size)
             randomX = rand() %9 + 1; //losowanie pozycji x i sprawdzenie czy nie wychodzi poza granice
             if(randomX + size > 9 && size>1)
             {
-                randomX = randomX - size; //jeœli wychodzi to statek "przesuwamy" bli¿ej
+                randomX = randomX - size; //jeÅ“li wychodzi to statek "przesuwamy" bliÂ¿ej
 
             }
             randomY = rand() %9 + 1; //losowanie pozycji y i sprawdzenie czy nie wychodzi poza granice
             if(randomY + size > 9 && size>1)
             {
-                randomY = randomY - size; //jeœli wychodzi to statek "przesuwamy" bli¿ej
+                randomY = randomY - size; //jeÅ“li wychodzi to statek "przesuwamy" bliÂ¿ej
             }
 
             //sprawdzamy kolizje z innymi statkami
@@ -102,7 +238,7 @@ int Ship::getSizeOfShip() const{return size;}
 
 Board::Board()
 {
-    //tworzenie podstawy mapy z wspó³rzednymi na rogach
+    //tworzenie podstawy mapy z wspÃ³lrzednymi na rogach
      map[0][0] = '/';
 
 		for(int i=1; i<10; i++)
@@ -137,10 +273,10 @@ bool Board::checkShipPlace(int randomX, int randomY, int isVertical, int size)
 
    if(isVertical)  // Statek poziomy
     {
-        // Sprawdzamy prostok¹t: szerokoœæ 3 pola (x-1, x, x+1) x wysokoœæ (size+2)
-        for(int i = -1; i <= 1; i++)  // Poziome s¹siedztwo
+        // Sprawdzamy prostokÂ¹t: szerokoÅ“Ã¦ 3 pola (x-1, x, x+1) x wysokoÅ“Ã¦ (size+2)
+        for(int i = -1; i <= 1; i++)  // Poziome sÂ¹siedztwo
         {
-            for(int j = -1; j <= size; j++)  // Pionowe s¹siedztwo
+            for(int j = -1; j <= size; j++)  // Pionowe sÂ¹siedztwo
             {
                 int checkX = randomX + i;
                 int checkY = randomY + j;
@@ -156,10 +292,10 @@ bool Board::checkShipPlace(int randomX, int randomY, int isVertical, int size)
     }
     else  // Statek pionowy
     {
-        // Sprawdzamy prostok¹t: szerokoœæ (size+2) x wysokoœæ 3 pola (y-1, y, y+1)
-        for(int i = -1; i <= size; i++)  // Poziome s¹siedztwo
+        // Sprawdzamy prostokÂ¹t: szerokoÅ“Ã¦ (size+2) x wysokoÅ“Ã¦ 3 pola (y-1, y, y+1)
+        for(int i = -1; i <= size; i++)  // Poziome sÂ¹siedztwo
         {
-            for(int j = -1; j <= 1; j++)  // Pionowe s¹siedztwo
+            for(int j = -1; j <= 1; j++)  // Pionowe sÂ¹siedztwo
             {
                 int checkX = randomX + i;
                 int checkY = randomY + j;
@@ -218,9 +354,9 @@ void Board::printShips()
     cout<<endl;
 }
 
-void autoShipPlacement(Board& board)
+bool Player::autoShipPlacement(Board& board)
 {
-    system("cls");
+
     cout<<"automatyczne wstawianie statkow\n";
 
     for(int i=4;i>0;i--)
@@ -229,9 +365,9 @@ void autoShipPlacement(Board& board)
     }
 
 }
-void manualShipPlacement(Board& board)
+bool Player::manualShipPlacement(Board& board)
 {
-    system("cls");
+
     cout<<"Wygenerowano mape, musisz teraz wstawic swoje statki:\n";
     cout<<"Do wyboru masz 4 rodzaje statkow:\n";
     cout<<"4 - polowy statek [1szt]\n";
@@ -260,6 +396,7 @@ int selectGameMode()
          {
             if (gameMode >= 1 && gameMode <= 2)
             {
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
                 break;
             }
             cout << "Liczba musi byc 1 lub 2!\n";
@@ -267,63 +404,17 @@ int selectGameMode()
          else
          {
             cout << "To nie jest liczba!\n";
-            cin.clear(); // Wyczyœæ flagê b³êdu
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Zignoruj resztê danych
+            cin.clear(); // WyczyÅ“Ã¦ flagÃª bÂ³Ãªdu
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Zignoruj resztÃª danych
          }
      }
 
     return gameMode;
 }
-void playVsSi()
-{
-    system("cls");
-    cout<<"wybrales gre z SI\n";
-    cout<<"Czy chcesz ustawic samodzielnie statki? t/n"<<endl;
-    char placeChoice;
-    while (true)
-    {
-         if (cin>>placeChoice)
-         {
-            if (placeChoice == 't' || placeChoice == 'n')
-            {
-                break;
-            }
-            cout << "musisz podac t lub n!\n";
-         }
-         else
-         {
-            cout << "To nie jest litera!\n";
-            cin.clear(); // Wyczyœæ flagê b³êdu
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Zignoruj resztê danych
-         }
-    }
-    Board playerBoard;
-    if(placeChoice == 't')
-    {
-        manualShipPlacement(playerBoard);
-    }
-    else
-    {
-        autoShipPlacement(playerBoard);
-        cout<<"plansza gracza: \n";
-        playerBoard.printMap();
-
-        cout<<endl;
 
 
 
-    }
-    Board siBoard;
-    autoShipPlacement(siBoard);
-    cout<<"statki si postawione pomyslnie!\n";
 
-    cout<<"plansza si: \n";
-    siBoard.printMap();
-}
-void playVsPlayer()
-{
-    cout<<"wybrales gre na dwie osoby\n";
-}
 
 int main(int argc, char** argv) {
 
@@ -341,12 +432,12 @@ int main(int argc, char** argv) {
 	 {
 	 	case 1:
 	 		{
-	 		    playVsSi();
+	 		   Game g1(1);
 			}
 	 		break;
 		case 2:
 	 		{
-	 		    playVsPlayer();
+	 		    cout<<"w trakcie tworzenia\n";
 	 		}
 	 		break;
 	 	default:
